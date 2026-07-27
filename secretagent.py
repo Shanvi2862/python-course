b7f47e74e737ff062cd61506207c72e95e779630
Shanvi2862 <sridevi.katta924@gmail.com>
2026-07-27 17:58:40 +1000

    Add files via upload

---
+++ secretagent.py
@@ -0,0 +1,23 @@
+name=input("enter your real name,agent")
+gadget=input("enter your favourite gadget")
+agent_num=3
+speedrating=3.13
+missioncount=3
+height_m=6.7
+is_active=True
+
+print("name",name," -> type:",type(name))
+print("gadget",gadget," -> type:",type(gadget))
+print("agent_num",agent_num," -> type:",type(agent_num))
+print("speedrating",speedrating," -> type:",type(speedrating))
+print("missioncount",missioncount," -> type:",type(missioncount))
+print("height_m",height_m," -> type:",type(height_m))
+print("is_active",is_active," -> type:",type(is_active))
+agent_number_text=str(agent_num)
+mission_count_text=str(missioncount)
+speedrating_text=str(speedrating)
+status_text=str(is_active)
+print(type(agent_number_text))
+print(type(mission_count_text))
+print(type(speedrating_text))
+print(type(status_text))
