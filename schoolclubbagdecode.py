d54164c66e55c0516b7f7ac9edad696efbe008a0
Shanvi2862 <sridevi.katta924@gmail.com>
2026-08-02 13:15:51 +1000

    Add files via upload

---
+++ schoolclubbagdecode.py
@@ -0,0 +1,15 @@
+membername=input("enert your member name")
+schoolclubname=input("enter your school club name")
+print("\n hello", membername,"your school club name is",schoolclubname)
+print("will generate a batch code")
+member_num=3
+points_earned=100000000
+activemember=True
+badge_code=member_num
+print(badge_code)
+print("membername->",type(membername))
+print("schoolclubname->",type(schoolclubname))
+print("member_num->",type(member_num))
+print("points_earned->",type(points_earned))
+print("activemember->",type(activemember))
+print("badge_code->",type(badge_code))
