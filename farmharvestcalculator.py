6d01c1fedad9987809f27ec3d773fb99b4361302
Shanvi2862 <sridevi.katta924@gmail.com>
2026-08-02 18:05:16 +1000

    Add files via upload

---
+++ farmharvestcalculator.py
@@ -0,0 +1,23 @@
+f1=438976
+f2=783596
+f3=9999999
+f4=3267546
+f5=90238437
+total=f1+f2+f3+f4+f5
+print("THIS IS YOUR TOTAL HARVEST",total,"kg")
+avarage=total//5
+print("THIS IS YOUR AVARAGE",avarage,"kg")
+priceforkg=15
+tamount=total*priceforkg
+print(tamount)
+bag=total//25
+leftover=total%25
+print("full bags packed",bag)
+print("left over grains",leftover)
+lyear=987976543213978756832863
+print("better than last year",total>lyear)
+print("same as last year",total==lyear)
+print("atleast as good",total>=lyear)
+print("less than last year",total<lyear)
+
+
