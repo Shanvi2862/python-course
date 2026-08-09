1bf12de294293783b1206dd9a4aa06fcc64b5793
Shanvi2862 <sridevi.katta924@gmail.com>
2026-08-09 14:55:11 +1000

    Add files via upload

---
+++ classroompointcalucator.py
@@ -0,0 +1,15 @@
+team1=10
+team2=30
+team3=100
+team4=50
+team5=40
+total= team1+team2+team3+team4+team5
+avarege= total / 5
+print(team1)
+print("total points scored by five teams is",total, "and average points scored is",avarege)
+stars_each_point=5
+total_stars= total*stars_each_point
+stars_each_pack=25
+total_packs=total / stars_each_pack
+leftoverstars=total%stars_each_pack
+print("totalpacksofstars",total_packs,"total leftover stars",leftoverstars)
