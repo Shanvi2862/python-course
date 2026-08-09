b04e8d8509704944416d590988f42f979b4930e9
Shanvi2862 <sridevi.katta924@gmail.com>
2026-08-09 17:50:59 +1000

    Add files via upload

---
+++ wheateroutfitpicker.py
@@ -0,0 +1,12 @@
+temp=int(input("enter todays temperature in celsius"))
+if temp < 20:
+    outfit="jacket"
+    print("it is cold today.")
+    print("wear a",outfit)
+else:
+    outfit="t-shirt"
+    print("it is warm today.")
+    print("wear a",outfit)
+
+
+ 
